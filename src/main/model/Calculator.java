8690008f7be34aa400a0aba81c940fba5abc08cf
kai <kai@kai.com>
2023-06-12 21:23:54 -0400

    calculator able to parse arithmetic expressions

--- src/main/model/Calculator.java
+++ src/main/model/Calculator.java
@@ -1,5 +1,22 @@
 package main.model;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+/*
+ * Calculator holds information about the input expression and the result of evaluating it.
+ * Arithmetic expression is parsed using the exp4j library:
+ *      https://github.com/fasseg/exp4j 
+ *      https://central.sonatype.com/search?q=g:net.objecthunter%20a:exp4j&smo=true 
+ * 
+ * Valid operations for ExpressionBuilder can be found in documentation:
+ *      https://www.objecthunter.net/exp4j/
+ *  
+ * 
+ * Calculator holds the true result of evaluating the expression. Intentionally making the result wrong is 
+ * not its responsibility. 
+ */
+
 
 public class Calculator {
     // member variables
@@ -17,7 +34,9 @@ public class Calculator {
 
     public void update(String newExpression) {
         this.expression = newExpression;
-        this.result = Double.parseDouble(newExpression);
+        Expression exp = new ExpressionBuilder(newExpression).build();
+        this.result = exp.evaluate();
+
     }
 
 
@@ -28,9 +47,6 @@ public class Calculator {
     public double getResult() {
         return this.result;
     }
-
-
-
 
 
 }
--- src/test/model/CalculatorTest.java
+++ src/test/model/CalculatorTest.java
@@ -1,6 +1,7 @@
 package test.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -33,15 +34,93 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testUpdate() {
+    public void testUpdateNumberInput_Success() {
         calc.update("1");
         assertEquals("1", calc.getExpression());
         assertEquals(1, calc.getResult(), delta);
 
-        // calc.update("1 + 0");
-        // assertEquals("1 + 0", calc.getExpression());
-        // assertEquals(1, calc.getResult(), delta);
+        calc.update("12.2");
+        assertEquals("12.2", calc.getExpression());
+        assertEquals(12.2, calc.getResult(), delta);
 
-        System.out.println("    - testUpdate passed");
+        calc.update("-44.4561");
+        assertEquals("-44.4561", calc.getExpression());
+        assertEquals(-44.4561, calc.getResult(), delta);
+
+        System.out.println("    - testUpdateNumberInput_Success passed");
     }
+
+    @Test
+    public void testUpdateNumberInput_Fail() {
+        try {
+            calc.update("www");
+            fail("Expected exception was not thrown.");
+        } catch (Exception e) {
+            assertEquals("www", calc.getExpression());
+            assertEquals(0, calc.getResult(), delta);
+        }
+
+
+        System.out.println("    - testUpdateNumberInput_Fail passed");
+
+    }
+
+
+    @Test
+    public void testUpdateExpressionInput_Success() {
+        calc.update("1 + 0");
+        assertEquals("1 + 0", calc.getExpression());
+        assertEquals(1, calc.getResult(), delta);
+
+        calc.update("5*4");
+        assertEquals("5*4", calc.getExpression());
+        assertEquals(20, calc.getResult(), delta);
+
+        calc.update("-2*3+2");
+        assertEquals("-2*3+2", calc.getExpression());
+        assertEquals(-4, calc.getResult(), delta);
+
+        calc.update("2(3)");
+        assertEquals("2(3)", calc.getExpression());
+        assertEquals(6, calc.getResult(), delta);
+
+        calc.update("-sqrt(4)");
+        assertEquals("-sqrt(4)", calc.getExpression());
+        assertEquals(-2, calc.getResult(), delta);
+
+        calc.update("11/11");
+        assertEquals("11/11", calc.getExpression());
+        assertEquals(1, calc.getResult(), delta);
+
+        calc.update("10/4");
+        assertEquals("10/4", calc.getExpression());
+        assertEquals(2.5, calc.getResult(), delta);
+
+
+        System.out.println("    - testUpdateExpressionInput_Success passed");
+    }
+
+
+    @Test
+    public void testUpdateExpressionInput_Fail() {
+        try {
+            calc.update("()");
+            fail();
+        } catch (Exception e) {
+            assertEquals("()", calc.getExpression());
+            assertEquals(0, calc.getResult(), delta);
+        }
+
+        try {
+            calc.update("10/0");
+            fail();
+        } catch (Exception e) {
+            assertEquals("10/0", calc.getExpression());
+            assertEquals(0, calc.getResult(), delta);
+        }
+
+
+        System.out.println("    - testUpdateExpressionInput_Fail passed");
+    }
+
 }
