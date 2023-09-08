d418d5c276d272b625bddf16133c466ce61520f0
unknown <kaikaialami@gmail.com>
2023-09-08 11:32:47 -0700

    created

--- src/main/ui/CalculatorPanel.java
+++ src/main/ui/CalculatorPanel.java
@@ -4,6 +4,11 @@ package main.ui;
 import javax.swing.*;
 import java.awt.*;
 
+
+/* Custom panel containing calculator (includes clickable buttons, speech bubbles, etc)
+ * 
+ * 
+ */
 public class CalculatorPanel extends JPanel {
     
 }
